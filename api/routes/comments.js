6ddb329d7cef297f4a81e7738be30a1b97c75097
iamajaysingh <iamajaysingh4@gmail.com>
2021-03-23 19:50:52 +0530

    fixed task validation error

--- api/routes/comments.js
+++ api/routes/comments.js
@@ -1,18 +1,12 @@
 const router = require("express").Router();
 const asyncMiddleware = require("../middleware/asyncMiddleware");
+
 const commentService = require("../service/comments");
 
 router.get(
   "/",
   asyncMiddleware(async (req, res) => {
     console.log(req.body);
-    ///const { error } = validateTask(req.body);
-    // if (error) {
-    //   return res
-    //     .send("request failed!!! reason" + error.details[0].message)
-    //     .status(400);
-    // }
-
     const result = await commentService.getComments();
     if (result) return res.send(result);
   })
